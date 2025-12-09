08b2f6cb84c6045e9f6568420c763d08174b424a
Ubaid-UllahGitHub <ubaidwaqar71@gmail.com>
2025-12-09 08:18:49 -0500

    Deploy to Netlify, add memebr section

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,8 @@ function App() {
       <CreativeTextSection />
       <ManifestoSection />
       <AnimatedMembers />
+      <Header />
+      <Hero />
       {/* <FooterSection /> */}
     </>
   );
